ce4de88f0fa434398dab10c1e07f279997779d18
H0wZy <h0wzymarcos@gmail.com>
2026-08-13 17:28:17 -0300

    fix: change param signature

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -26,7 +26,7 @@ type UserService interface {
 	FindByEmail(ctx context.Context, email string) (*model.User, error)
 	FindByID(ctx context.Context, id uint) (*model.User, error)
 	List(ctx context.Context) ([]*model.User, error)
-	Update(ctx context.Context, user *model.User) error
+	Update(ctx context.Context, id uint, input *dto.UpdateUser) error
 	Delete(ctx context.Context, id uint) error
 }
 
@@ -91,7 +91,7 @@ func (s *userService) List(ctx context.Context) ([]*model.User, error) {
 	panic("unimplemented")
 }
 
-func (s *userService) Update(ctx context.Context, user *model.User) error {
+func (s *userService) Update(ctx context.Context, id uint, input *dto.UpdateUser) error {
 	panic("unimplemented")
 }
 
